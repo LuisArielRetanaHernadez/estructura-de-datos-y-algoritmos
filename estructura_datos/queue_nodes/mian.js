540e22717d4448aef8e08756e48cff7b4b4a44c6
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-05 18:30:38 -0600

    crneo el metoo enqueue con el parametro value

--- estructura_datos/queue_nodes/mian.js
+++ estructura_datos/queue_nodes/mian.js
@@ -11,4 +11,16 @@ class Stack {
     this.front = null;
     this.length = 0;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.rear) {
+      this.rear = newNode;
+      this.front = newNode;
+    } else {
+      this.rear.prev = newNode;
+      this.rear = newNode;
+    }
+    this.length++;
+  }
 }
