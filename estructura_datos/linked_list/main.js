275772676e8e3f9c3672b7352c1df8cca7819e67
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-14 19:59:25 -0600

    creando el metodo add donde agregamos un nuevo node al linkedList

--- estructura_datos/linked_list/main.js
+++ estructura_datos/linked_list/main.js
@@ -9,4 +9,14 @@ class LinkedList {
   constructor() {
     this.head = null
   }
+
+  add(value) {
+    const node = new Node(value)
+    if (this.head === null) {
+      this.head = node
+    } else {
+      node.next = this.head
+      this.head = node
+    }
+  }
 }
